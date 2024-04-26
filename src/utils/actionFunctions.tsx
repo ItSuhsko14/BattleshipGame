b1cfe23c5b41e6b549b4ea468fed08b32b17cca5
ItSushko14 <itsushkoandriy@gmail.com>
2024-04-26 18:15:22 +0300

    clear code from trash

--- src/utils/actionFunctions.tsx
+++ src/utils/actionFunctions.tsx
@@ -4,45 +4,45 @@ export const computerShoot = (userField: Field, prevShot: { i: number, j: number
     console.log('--- computerShoot ---');
 
     let i, j;
-    // if (prevShot) {
-    //     const offsets = [
-    //         { row: -1, col: 0 }, // вище
-    //         { row: 1, col: 0 },  // нижче
-    //         { row: 0, col: -1 }, // ліворуч
-    //         { row: 0, col: 1 },  // праворуч
-    //     ];
+    if (prevShot = 1) {
+        const offsets = [
+            { row: -1, col: 0 }, // вище
+            { row: 1, col: 0 },  // нижче
+            { row: 0, col: -1 }, // ліворуч
+            { row: 0, col: 1 },  // праворуч
+        ];
 
-    //     // Масив для зберігання доступних для вибору сусідніх клітинок
-    //     const availableNeighbors: { i: number, j: number }[] = [];
+        // Масив для зберігання доступних для вибору сусідніх клітинок
+        const availableNeighbors: { i: number, j: number }[] = [];
 
-    //     for (const offset of offsets) {
-    //         const adjacentRow = prevShot.i + offset.row;
-    //         const adjacentCol = prevShot.j + offset.col;
+        for (const offset of offsets) {
+            const adjacentRow = prevShot.i + offset.row;
+            const adjacentCol = prevShot.j + offset.col;
 
-    //         // Перевірка, чи координати сусідньої клітинки в межах поля
-    //         if (
-    //             adjacentRow >= 0 && adjacentRow < userField.length &&
-    //             adjacentCol >= 0 && adjacentCol < userField[0].length
-    //         ) {
-    //             const neighborCell = userField[adjacentRow][adjacentCol];
+            // Перевірка, чи координати сусідньої клітинки в межах поля
+            if (
+                adjacentRow >= 0 && adjacentRow < userField.length &&
+                adjacentCol >= 0 && adjacentCol < userField[0].length
+            ) {
+                const neighborCell = userField[adjacentRow][adjacentCol];
 
-    //             // Перевірка, чи сусідня клітинка ще не потрапила
-    //             if (neighborCell.mode !== 'missed' && neighborCell.mode !== 'hit') {
-    //                 console.log(availableNeighbors);
-    //                 availableNeighbors.push({ i: adjacentRow, j: adjacentCol });
-    //             }
-    //         }
-    //     }
+                // Перевірка, чи сусідня клітинка ще не потрапила
+                if (neighborCell.mode !== 'missed' && neighborCell.mode !== 'hit') {
+                    console.log(availableNeighbors);
+                    availableNeighbors.push({ i: adjacentRow, j: adjacentCol });
+                }
+            }
+        }
 
-        // Якщо є доступні сусідні клітинки, обираємо випадкову з них
-    //     console.log(availableNeighbors);
-    //     if (availableNeighbors.length > 0) {
-    //         const randomIndex = Math.floor(Math.random() * availableNeighbors.length);
-    //         console.log(randomIndex);
-    //         console.log(prevShot);        
-    //         return availableNeighbors[randomIndex];
-    //     }
-    // }
+        Якщо є доступні сусідні клітинки, обираємо випадкову з них
+        console.log(availableNeighbors);
+        if (availableNeighbors.length > 0) {
+            const randomIndex = Math.floor(Math.random() * availableNeighbors.length);
+            console.log(randomIndex);
+            console.log(prevShot);        
+            return availableNeighbors[randomIndex];
+        }
+    }
 
     do {
         const randomI = Math.floor(Math.random() * 10);
